a16caeb55a3fc449b35a90efcbefb62a452bd0c5
FlavioKawan <kawanflavio66@gmail.com>
2023-05-10 23:47:01 -0300

    Segunda versao

---
+++ fun.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+const senhaInput = document.getElementById('campo_senha');
+const btnMostrar = document.getElementById('btn_mostrar');
+const imgMostrar = btnMostrar.querySelector('.olho');
+
+btnMostrar.addEventListener('click', function() {
+  if (senhaInput.type === 'password') {
+    senhaInput.type = 'text';
+    imgMostrar.src = 'assets/olho-oculto.png';
+    imgMostrar.alt = 'Ocultar Senha';
+  } else {
+    senhaInput.type = 'password';
+    imgMostrar.src = 'assets/olho.png';
+    imgMostrar.alt = 'Mostrar Senha';
+  }
+	});
+});
